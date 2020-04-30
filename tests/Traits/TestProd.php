ac6e0f6b2e4baf25574ed4bd785b6b6beb4c9901
Paulo Silva <paulo.silva@mobly.com.br>
2020-04-30 12:36:38 -0300

    make skip testing prod

---
+++ tests/Traits/TestProd.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Traits;
+
+
+trait TestProd
+{
+
+    protected function skipTestIfNotProd($message = '')
+    {
+        if (!$this->isTestingProd()) {
+            $this->markTestSkipped($message);
+        }
+    }
+
+    protected function isTestingProd(): bool
+    {
+        return env('TESTING_PROD') !== false;
+    }
+
+}
